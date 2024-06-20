89dad99f7dcee33a8095e9e03195b5dfb1b6fd8d
Georgy <gbvolkov@gmail.com>
2024-06-20 10:25:06 +0300

    1

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-ELEVENLABS_API_KEY = "sk_94020e4e95c4d8330b6ba0f68a846e8b5d796a97a3a311bc"
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.path.expanduser("~/Documents"), 'gv.env'))
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+ELEVENLABS_API_KEY = os.environ.get('ELEVENLABS_API_KEY')
 TELEGRAM_BOT_TOKEN = "7429150554:AAE4px6aE3_EDgJU-Yu0NSPe55wv4ixqbv4"
